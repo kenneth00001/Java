1217e8b438ade9014e76586f3e84abc6adcea632
Kenneth Samuel <103204100+kenneth00001@users.noreply.github.com>
2022-11-11 11:56:22 -0400

    ok

---
+++ ex12.js
@@ -0,0 +1,23 @@
+var agora = new Date()
+var dia = agora.getDay()
+
+switch(dia) {
+    case 0:
+    console.log("Domingo")
+    break
+    case 1:
+    console.log("Hoje é segunda-Feira")
+    break
+    case 2:
+    console.log("Hoje é terça-Feira")
+    break
+    case 3:
+    console.log("Hoje é Quarta-Feira")
+    break
+    case 4:
+    console.log("Hoje é Quinta-Feira")
+    break
+    case 5:
+    console.log("Hoje é Sexta-Feira")
+    break
+}
